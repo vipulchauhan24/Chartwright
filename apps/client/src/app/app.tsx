6b98183118cb76e0e7e43c4edb4d531de93f09b5
vipulchauhan24 <vipul9556@gmail.com>
2025-03-21 12:05:13 +0530

    Implemented congnito OIDC.

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,16 +1,57 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import ChartEditor from './charteditor';
 import RenderChart from './renderChart';
+import { useAuth } from 'react-oidc-context';
+import CWButton from './components/button';
+import { useEffect } from 'react';
+import Spinner from './components/spinner';
 
 export function App() {
+  const auth = useAuth();
+
+  const continueAsGuest = () => {
+    console.log('continueAsGuest');
+  };
+
+  const continueAsUser = () => {
+    auth.signinRedirect();
+  };
+
+  if (auth.isLoading) {
+    return (
+      <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (!auth.isAuthenticated) {
+    return (
+      <div className="h-full w-full flex items-center justify-center bg-primary-background">
+        <div className="flex gap-2">
+          <CWButton label="Continue as guest" onClick={continueAsGuest} />
+          <CWButton label="Continue as user" onClick={continueAsUser} />
+        </div>
+      </div>
+    );
+  }
+
+  const RedirectToChartUrl = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      navigate('/chart');
+    });
+
+    return null;
+  };
+
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<ChartEditor />} />
-        <Route path="/chart/render/:id" element={<RenderChart />} />
-      </Routes>
-      {/* END: routes */}
-    </>
+    <Routes>
+      <Route path="/" element={<RedirectToChartUrl />} />
+      <Route path="/chart" element={<ChartEditor />} />
+      <Route path="/chart/render/:id" element={<RenderChart />} />
+    </Routes>
   );
 }
 
--- apps/client/src/app/charteditor/layout/appheader.tsx
+++ apps/client/src/app/charteditor/layout/appheader.tsx
@@ -1,13 +1,39 @@
+import { useAuth } from 'react-oidc-context';
 import CWLink from '../../components/link';
+import CWButton from '../../components/button';
+
+const { VITE_CLIENT_ID } = import.meta.env;
 
 function AppHeader() {
+  const auth = useAuth();
+
+  const redirectToLoginPage = () => {
+    auth.signinRedirect();
+  };
+
+  const logout = async () => {
+    await auth.signoutRedirect({
+      extraQueryParams: {
+        client_id: VITE_CLIENT_ID,
+        logout_uri: window.location.origin,
+      },
+    });
+  };
+
   return (
-    <header className="h-[4.5rem] border-b border-primary-border py-2 px-4 flex items-start">
+    <header className="h-[4.5rem] border-b border-primary-border py-2 px-4 flex items-center justify-between">
       <CWLink
         href="/"
         nohover
         label={<img src="/cw-logo.png" alt="chart wright" className="h-12" />}
       />
+      <div>
+        {auth.isAuthenticated ? (
+          <CWButton label="Sign out" onClick={logout} />
+        ) : (
+          <CWButton label="Sign in" onClick={redirectToLoginPage} />
+        )}
+      </div>
     </header>
   );
 }
